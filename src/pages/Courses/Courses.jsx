902a50e34524d798013c54723f74b213354c81af
g0ks <goksrofficial@gmail.com>
2024-08-31 17:15:55 +0530

    profile page started

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,62 +1,87 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 function Courses({ setShow, setSliderShow }) {
   setShow(true);
   setSliderShow(true);
   return (
-    
-    <Container  >
-        <div className='d-flex justify-content-center align-items-center ' style={{paddingTop:"100px", marginBottom:"50px"}} >
-            <h2>Courses</h2>
-        </div>
-      <Row className='gap-3' >
-        <Col className='d-none d-lg-block'></Col>
-        <Col className='d-flex justify-content-center align-items-center '>
+    <Container>
+      <div
+        className="d-flex"
+        style={{
+          paddingTop: '100px',
+          marginBottom: '50px',
+          position: 'relative',
+        }}
+      >
+        <div
+          className="shape d-flex align-items-start mt-1"
+          style={{ position: 'absolute', left: '248px' }}
+        ></div>
+        <h2 className="fs-2 p-2" style={{ position: 'absolute', left: '254px', top:"90px" , }}>Courses</h2>
+      </div>
+      <Row className="gap-3">
+        <Col className="d-none d-lg-block"></Col>
+        <Col className="d-flex justify-content-center align-items-center   ">
           <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
+            <Card.Body className="p-4 d-flex align-items-center justify-content-center flex-column ">
+              <Card.Title>Course name </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted mt-2  ">
+                University
               </Card.Subtitle>
+              <Card.Text className="">country</Card.Text>
+              <Card.Text className="">deadline</Card.Text>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                <Card.Text className="">scholarship : yes/no</Card.Text>
               </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <Button
+                className="btn text-light "
+                style={{ backgroundColor: '#0F6990' }}
+              >
+                Course Link
+              </Button>
             </Card.Body>
           </Card>
         </Col>
-        <Col className='d-flex justify-content-center align-items-center '>
+        <Col className="d-flex justify-content-center align-items-center ">
           <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
+            <Card.Body className="p-4 d-flex align-items-center justify-content-center flex-column">
+              <Card.Title>Course name </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted mt-2  ">
+                University
               </Card.Subtitle>
+              <Card.Text className="">country</Card.Text>
+              <Card.Text className="">deadline</Card.Text>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                <Card.Text className="">scholarship : yes/no</Card.Text>
               </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <Button
+                className="btn text-light "
+                style={{ backgroundColor: '#0F6990' }}
+              >
+                Course Link
+              </Button>
             </Card.Body>
           </Card>
-        </Col >
-        <Col className='d-flex justify-content-center align-items-center'>
+        </Col>
+        <Col className="d-flex justify-content-center align-items-center">
           <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
+            <Card.Body className="p-4 d-flex align-items-center justify-content-center flex-column">
+              <Card.Title>Course name </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted mt-2  ">
+                University
               </Card.Subtitle>
+              <Card.Text className="">country</Card.Text>
+              <Card.Text className="">deadline</Card.Text>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                <Card.Text className="">scholarship : yes/no</Card.Text>
               </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <Button
+                className="btn text-light "
+                style={{ backgroundColor: '#0F6990' }}
+              >
+                Course Link
+              </Button>
             </Card.Body>
           </Card>
         </Col>
