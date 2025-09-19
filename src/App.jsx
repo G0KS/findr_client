103471ab1283af910c4b9a4df1d146147e2a010f
g0ks <goksrofficial@gmail.com>
2025-09-19 13:02:01 +0530

    ui changes in home

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ import PagenotFound from "./pages/PagenotFound/PagenotFound.jsx";
 import { FrappeProvider } from "frappe-react-sdk";
 import SliderComponent from "./components/SliderComponent/SliderComponent.jsx";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword.jsx";
-import Test from "./pages/Test.jsx";
 
 function App() {
    const [show, setShow] = useState(true);
@@ -46,12 +45,6 @@ function App() {
             {sidebarShow && <SliderComponent />}
             <ScrollToTop />
             <Routes>
-               <Route
-                  path="/test"
-                  element={
-                     <Test setShow={setShow} setSidebarShow={setSidebarShow} />
-                  }
-               />
                <Route
                   path="/"
                   element={
